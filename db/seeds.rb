2c843d60d3ad6da79707665754b11e5a8c842598
ebeecroft1 <eric.beecroft@gmail.com>
2022-05-19 09:22:42 +1000

    Players seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,11 @@ p7 = Player.create :name => "LeBron James", :position => 'SF', :age => '37', :he
 p8 = Player.create :name => "Ja Morant", :position => 'PG', :age => '22', :height => "6'3", :weight => '174', :points => '27.4', :rebounds => '5.7', :assists => '6.7', :blocks => '0.4', :steals => '1.2', :college => 'Murray State', :image => 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4279888.png&w=350&h=254'
 p9 = Player.create :name => "DeMar DeRozan", :position => 'SF', :age => '32', :height => "6'6", :weight => '220', :points => '27.9', :rebounds => '5.2', :assists => '4.9', :blocks => '0.3', :steals => '0.9', :college => 'USC', :image => 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3978.png&w=350&h=254'
 p10 = Player.create :name => "Chris Paul", :position => 'PG', :age => '36', :height => "6'0", :weight => '175', :points => '14.7', :rebounds => '4.4', :assists => '10.8', :blocks => '0.3', :steals => '1.9', :college => 'Wake Forest', :image => 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/2779.png&w=350&h=254'
-# Add 15 next players
+p11 = Player.create :name => "Jimmy Butler", :position => 'SF', :age => '32', :height => "6'7", :weight => '230', :points => '21.4', :rebounds => '5.9', :assists => '5.5', :blocks => '0.5', :steals => '1.6', :college => 'Marquette', :image => 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6430.png&w=350&h=254'
+p12 = Player.create :name => "Jayson Tatum", :position => 'SF', :age => '24', :height => "6'8", :weight => '210', :points => '26.9', :rebounds => '8.0', :assists => '4.4', :blocks => '0.6', :steals => '1.0', :college => 'Duke', :image => 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4065648.png&w=350&h=254'
+p13 = Player.create :name => "Devin Booker", :position => 'SG', :age => '25', :height => "6'5", :weight => '206', :points => '26.8', :rebounds => '5.0', :assists => '4.8', :blocks => '0.4', :steals => '1.1', :college => 'Kentucky', :image => 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3136193.png&w=350&h=254'
+p14 = Player.create :name => "Donovan Mitchell", :position => 'SG', :age => '25', :height => "6'1", :weight => '215', :points => '25.9', :rebounds => '4.2', :assists => '5.3', :blocks => '0.2', :steals => '1.5', :college => 'Louisville', :image => 'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3908809.png&w=350&h=254'
+# Add 11 next players
 puts "#{ Player.count } players"
 
 Team.destroy_all
